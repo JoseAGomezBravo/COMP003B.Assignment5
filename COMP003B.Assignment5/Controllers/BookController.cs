df9274d549c9db9b1c55b77f053005962d96cb12
JoseAGomezBravo <04josegomez2022@gmail.com>
2024-04-24 20:00:00 -0700

    Add project files.

---
+++ COMP003B.Assignment5/Controllers/BookController.cs
@@ -0,0 +1,39 @@
+﻿using COMP003B.Assignment5.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment5.Controllers
+{
+    [ApiController]
+    [Route("api/[controller")]
+    public class BookController : Controller
+    {
+        private List<Book> _books = new List<Book>();
+
+        public BookController() {
+            _books.Add(new Book { Id = 1, Name = "Into the Wild", Description = "Adventure", Author = "Jon Krakauer" });
+            _books.Add(new Book { Id = 2, Name = "The Lightning Thief", Description = "Adventure/Magic", Author = "Rick Riordan" });
+            _books.Add(new Book { Id = 3, Name = "The Sea of Monsters", Description = "Adventure/Magic", Author = "Rick Riordan" });
+            _books.Add(new Book { Id = 4, Name = "The Titan's Curse", Description = "Adventure/Magic", Author = "Rick Riordan" });
+            _books.Add(new Book { Id = 5, Name = "The Last Olympian", Description = "Adventure/Magic", Author = "Rick Riordan" });
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> GetAllBooks() 
+        {
+            return _books;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Book> GetBookbyId(int id) 
+        {
+            var book = _books.FirstOrDefault(v => v.Id == id);
+
+            if (book == null) 
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+    }
+}
---
+++ COMP003B.Assignment5/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc.Formatters;
+using System.Xml.Serialization;
+
+namespace COMP003B.Assignment5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers(options =>
+            {
+                options.InputFormatters.Add(new XmlSerializerInputFormatter(options));
+                options.OutputFormatters.Add(new XmlSerializerOutputFormatter());
+            });
+          
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
